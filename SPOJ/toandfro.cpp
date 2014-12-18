f90c0e6caef97e190eaf765241d385eb4b5174f5
pattu777 <patanaikchinmaya@gmail.com>
2014-12-18 14:22:14 +0100

    to and fro

--- SPOJ/toandfro.cpp
+++ SPOJ/toandfro.cpp
@@ -1,29 +1,41 @@
-#include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <math.h>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int main(int argc, char** argv)
+void decrypt_msg(int clm, string msg)
 {
-    fstream fin;
-    fin.open(argv[1]);
-    string line;
+    vector<string> vec;
+    for(int i=0; i<msg.size(); i+=clm)
+        vec.push_back(msg.substr(i, clm));
 
-    getline(fin, line);
-    int testcases = atoi(line.c_str());
-
-    while(testcases--)
+    for(int i=0; i<clm; i++)
     {
-        getline(fin,line);
-        stringstream linestream(line);
-        int num;
-        linestream >> num;
+        for(int j=0; j<vec.size(); j++)
+        {
+            if((j%2)==0)
+                cout << vec[j][i];
+            else
+                cout << vec[j][clm-i-1];
+        }
+    }
+    cout << endl;
+}
+
+int main(void)
+{
+    while(1)
+    {
+        string line;
+        getline(cin, line);
+        int clm = atoi(line.c_str());
+        if(clm == 0)
+            break;
+
+        getline(cin,line);
+        decrypt_msg(clm, line);
     }
     return 0;
 }
